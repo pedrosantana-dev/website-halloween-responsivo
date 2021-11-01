11034c684932e4279eaf6a2293a4aca7ec0c4c30
Pedro Santana <pedromanu27@gmail.com>
2021-11-01 00:35:14 -0300

    Scroll reveal animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -84,3 +84,16 @@ function scrollUp(){
 }
 window.addEventListener('scroll', scrollUp);
 
+/* SCROLL REVEAL ANIMATION */
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: '2500',
+    delay: 400,
+    // reset: true
+});
+
+sr.reveal('.home-swiper, .new-swiper, .newsletter__container')
+sr.reveal('.category__data, .trick__content, .footer__content', {interval: 100})
+sr.reveal('.about__data, .discount__img', {origin: 'left'})
+sr.reveal('.about__img, .discount__data', {origin: 'right'})
